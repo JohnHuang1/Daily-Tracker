b1f793cfeb6b01053a0d2b152792e4d72b5fac36
John Huang <46700107+JohnHuang1@users.noreply.github.com>
2021-06-17 17:10:01 -0400

    fixed flutter-side ios data storage for widget

--- lib/models/IOSWidgetData.dart
+++ lib/models/IOSWidgetData.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+
 
 import 'package:daily_tracker/models/streak.dart';
 
@@ -6,10 +6,12 @@ class IOSWidgetData{
   final List<Streak> streakList;
 
   IOSWidgetData(this.streakList);
+  Map<String, dynamic> toJson() {
 
-  // Map<String, dynamic> toJson() => Map.fromIterable(streakList, key: (e) => streakList.indexOf(e as Streak).toString(), value: (e) => (e as Streak).toJsonWidgetData());
+    List<Map> str = streakList.isNotEmpty ? streakList.map((streak) => streak.toJsonWidgetData()).toList() : null;
 
-  Map<String, dynamic> toJson() => {"streakList": jsonEncode(streakList)};
-  // IOSWidgetData.fromJson(Map<String, dynamic> json)
-  //   : streakList = json.entries.forEach()
+    return {
+      "streakList": str
+    };
+  }
 }
